c30460770fd6854c1245a3cb5856d2aea54780a8
Joshua Aclan <josh.aclan@gmail.com>
2026-04-08 20:24:30 +0800

    [IMP]ida_sales: Remove shipping address on SO portal

--- ida_sales/__manifest__.py
+++ ida_sales/__manifest__.py
@@ -17,6 +17,7 @@
     'data': [
         'data/ir_sequence_data.xml',
         'views/sale_order_views.xml',
+        'views/sale_portal_templates.xml',
     ],
     'installable': True,
     'application': False,
