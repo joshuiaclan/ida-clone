219c2d77a4f99d7131b45bf2ea0553ead19fc993
joshuiaclan <josh.aclan@gmail.com>
2026-04-29 18:04:05 +0800

    [FIX ERR]

--- ida_project/__manifest__.py
+++ ida_project/__manifest__.py
@@ -13,8 +13,7 @@
         'security/ir.model.access.csv',
         'data/ir_sequence_data.xml',
         'views/ida_base_project_views.xml',
-        'views/project_project_views.xml',
-        'views/sale_order_views.xml',
+        'views/project_project_views.xml'
     ],
     'installable': True,
     'application': False,
