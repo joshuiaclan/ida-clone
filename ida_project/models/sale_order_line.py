f6eabce30e153849929954284d213d72830450f3
Joshua Aclan <josh.aclan@gmail.com>
2026-04-29 22:31:22 +0800

    [FIX]ssdasd

--- ida_project/models/sale_order_line.py
+++ ida_project/models/sale_order_line.py
@@ -13,3 +13,19 @@ class SaleOrderLine(models.Model):
         if order.base_project_id and order.deal_type in ('new_project', 'existing_project'):
             values['name'] = order.base_project_id.number
         return values
+
+    def _timesheet_create_project(self):
+        """Skip project creation entirely for additional_services orders.
+
+        The SO is connected to an existing base project; no new subproject
+        should be created regardless of the product's service_tracking setting.
+        """
+        if self.order_id.deal_type == 'additional_services':
+            return self.env['project.project']
+        return super()._timesheet_create_project()
+
+    def _timesheet_create_task(self, project):
+        """Skip task creation entirely for additional_services orders."""
+        if self.order_id.deal_type == 'additional_services':
+            return self.env['project.task']
+        return super()._timesheet_create_task(project)
